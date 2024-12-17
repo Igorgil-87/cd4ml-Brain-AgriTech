eb086e4e80f2a47b61eabb6b89adbcc890a5b7f2
Igorgil-87 <igorhnlg@gmail.com>
2024-12-17 11:15:54 -0300

    subindo

--- cd4ml/problems/available_problems.py
+++ cd4ml/problems/available_problems.py
@@ -3,38 +3,10 @@ import cd4ml.problems.rendimento.problem as rendimento_scenario
 import cd4ml.problems.saude_lavoura.problem as saude_lavoura_scenario
 from cd4ml.problems.commodities.problem import CommoditiesProblem
 
-# Dicionário de problemas atualizados
+# Dicionário de problemas com referências às classes
 PROBLEMS = {
-    "insumo": insumo_scenario.Problem(
-        problem_name="insumo",
-        data_downloader="default",
-        ml_pipeline_params_name="default",
-        feature_set_name="default",
-        algorithm_name="default",
-        algorithm_params_name="default"
-    ),
-    "rendimento": rendimento_scenario.Problem(
-        problem_name="rendimento",
-        data_downloader="default",
-        ml_pipeline_params_name="default",
-        feature_set_name="default",
-        algorithm_name="random_forest",
-        algorithm_params_name="default"
-    ),
-    "saude_lavoura": saude_lavoura_scenario.Problem(
-        problem_name="saude_lavoura",
-        data_downloader="default",
-        ml_pipeline_params_name="default",
-        feature_set_name="default",
-        algorithm_name="random_forest",
-        algorithm_params_name="default"
-    ),
-    "commodities": CommoditiesProblem(
-        problem_name="commodities",
-        data_downloader="default",
-        ml_pipeline_params_name="default",
-        feature_set_name="default",
-        algorithm_name="default",
-        algorithm_params_name="default"
-    )
+    "insumo": insumo_scenario.Problem,  # Referência à classe
+    "rendimento": rendimento_scenario.Problem,  # Referência à classe
+    "saude_lavoura": saude_lavoura_scenario.Problem,  # Referência à classe
+    "commodities": CommoditiesProblem  # Referência à classe
 }
